59652a1d7213f4d357279ef8674b8211b54e7c2b
unknown <JeanThirion>
2023-11-13 11:32:04 +1100

    added company table

--- firebootcamp-crm/src/app/company/company-list/company-list.component.ts
+++ firebootcamp-crm/src/app/company/company-list/company-list.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class CompanyListComponent {
 
+  companies: any = [
+    { name: 'Company A', email:'companyA@ssw.com.au', phone: 12345 },
+    { name: 'Company B', email:'companyB@ssw.com.au', phone: 34567 },
+    { name: 'Company C', email:'companyC@ssw.com.au', phone: 45678 }
+  ]
 }
